5020769d789307438aed52b81e94a78942f415f9
Joshua Calonge <joshua.calonge@ziaplex.biz>
2023-07-19 09:13:07 +0800

    Scanner UI/UX
    added util files

--- app/src/main/java/com/lionscare/app/utils/NetworkExtensions.kt
+++ app/src/main/java/com/lionscare/app/utils/NetworkExtensions.kt
@@ -1,5 +1,9 @@
 package com.lionscare.app.utils
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.asRequestBody
@@ -12,4 +16,35 @@ fun File.asNetWorkRequestBody(mimeType: String): RequestBody {
 
 fun String?.createPartFromString(): RequestBody {
     return this.orEmpty().toRequestBody("text/plain".toMediaTypeOrNull())
+}
+
+fun isNetworkAvailable(context: Context): Boolean {
+    var result = false
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val networkCapabilities = connectivityManager.activeNetwork ?: return false
+        val actNw =
+            connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+        result = when {
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+    } else {
+        connectivityManager.run {
+            connectivityManager.activeNetworkInfo?.run {
+                result = when (type) {
+                    ConnectivityManager.TYPE_WIFI -> true
+                    ConnectivityManager.TYPE_MOBILE -> true
+                    ConnectivityManager.TYPE_ETHERNET -> true
+                    else -> false
+                }
+
+            }
+        }
+    }
+
+    return result
 }
---
+++ app/src/main/java/com/lionscare/app/utils/dialog/ScannerDialog.kt
@@ -0,0 +1,130 @@
+package com.lionscare.app.utils.dialog
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.fragment.app.DialogFragment
+import com.google.zxing.Result
+import com.lionscare.app.databinding.DialogScannerViewBinding
+import com.lionscare.app.R
+import com.lionscare.app.utils.CommonLogger
+import com.lionscare.app.utils.PermissionChecker
+import com.lionscare.app.utils.setOnSingleClickListener
+import me.dm7.barcodescanner.zxing.ZXingScannerView
+
+class ScannerDialog: DialogFragment(), ZXingScannerView.ResultHandler {
+
+    private var viewBinding: DialogScannerViewBinding? = null
+    private var callback: ScannerListener? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.dialog_scanner_view,
+            container,
+            false
+        )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.MATCH_PARENT
+        )
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.setCancelable(true)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewBinding = DialogScannerViewBinding.bind(view)
+        setClickListener()
+    }
+
+    private fun setClickListener(){
+        viewBinding?.dismissImageView?.setOnSingleClickListener {
+            dismiss()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (PermissionChecker.checkPermissions(requireActivity(), Manifest.permission.CAMERA, PERMISSION_CAMERA)) {
+            startCamera()
+        }
+    }
+
+    private fun startCamera() {
+        viewBinding?.scannerView?.setResultHandler(this)
+        viewBinding?.scannerView?.startCamera()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        viewBinding?.scannerView?.stopCamera()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewBinding?.scannerView?.stopCamera()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_CAMERA) {
+            if (isAllPermissionResultGranted(grantResults)) {
+                startCamera()
+            } else {
+                dismiss()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        viewBinding = null
+    }
+
+    private fun isAllPermissionResultGranted(grantResults: IntArray): Boolean {
+        var granted = true
+        for (grantResult in grantResults) {
+            if (grantResult == PackageManager.PERMISSION_DENIED) {
+                granted = false
+                break
+            }
+        }
+        return granted
+    }
+
+    interface ScannerListener {
+        fun onScannerSuccess(qrValue: String)
+    }
+
+    companion object {
+        fun newInstance(callback: ScannerListener? = null, title: String?=null) = ScannerDialog()
+            .apply {
+                this.callback = callback
+                this.viewBinding?.scannerTitleTextView?.text = title
+            }
+
+        val TAG: String = ScannerDialog::class.java.simpleName
+        private const val PERMISSION_CAMERA: Int = 787
+    }
+
+    override fun handleResult(rawResult: Result?) {
+        CommonLogger.devLog(TAG, "QR CODE: ${rawResult?.text}")
+        callback?.onScannerSuccess(rawResult?.toString().orEmpty())
+        dismiss()
+    }
+}
