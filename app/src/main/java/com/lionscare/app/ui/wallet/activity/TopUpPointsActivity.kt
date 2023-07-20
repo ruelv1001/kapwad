b503e8b928856e9780bb191d8a5f39c4845d413a
Joshua Calonge <joshua.calonge@ziaplex.biz>
2023-07-20 13:18:10 +0800

    put buttons in recyclerview

--- app/src/main/java/com/lionscare/app/ui/wallet/activity/TopUpPointsActivity.kt
+++ app/src/main/java/com/lionscare/app/ui/wallet/activity/TopUpPointsActivity.kt
@@ -4,19 +4,25 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import com.lionscare.app.R
+import com.lionscare.app.data.model.AddFundsModel
 import com.lionscare.app.databinding.ActivityTopUpPointsBinding
+import com.lionscare.app.ui.wallet.adapter.AddFundsAdapter
 import com.lionscare.app.utils.setOnSingleClickListener
 
-class TopUpPointsActivity : AppCompatActivity()  {
+class TopUpPointsActivity : AppCompatActivity(), AddFundsAdapter.AddFundCallback {
 
     private lateinit var binding: ActivityTopUpPointsBinding
+    private var gridLayoutManager: GridLayoutManager? = null
+    private var adapter: AddFundsAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTopUpPointsBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        setupAddFundsList()
         setupClickListener()
     }
 
@@ -24,30 +30,34 @@ class TopUpPointsActivity : AppCompatActivity()  {
         backImageView.setOnSingleClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+    }
 
-        h1Button.setOnSingleClickListener {
-            pointsEditText.setText(getString(R.string.top_up_h1_text))
-        }
-        h2Button.setOnSingleClickListener {
-            pointsEditText.setText(getString(R.string.top_up_h2_text))
-        }
-        h5Button.setOnSingleClickListener {
-            pointsEditText.setText(getString(R.string.top_up_h5_text))
-        }
-        k1Button.setOnSingleClickListener {
-            pointsEditText.setText(getString(R.string.top_up_k1_text))
-        }
-        k2Button.setOnSingleClickListener {
-            pointsEditText.setText(getString(R.string.top_up_k2_text))
-        }
-        k10Button.setOnSingleClickListener {
-            pointsEditText.setText(getString(R.string.top_up_k10_text))
-        }
+    private fun setupAddFundsList() = binding.run {
+        adapter = AddFundsAdapter(this@TopUpPointsActivity, this@TopUpPointsActivity)
+        gridLayoutManager = GridLayoutManager(this@TopUpPointsActivity, 4)
+        recyclerView.layoutManager = gridLayoutManager
+        recyclerView.adapter = adapter
+
+        val fundList = listOf(
+            AddFundsModel("100"),
+            AddFundsModel("200"),
+            AddFundsModel("500"),
+            AddFundsModel("1,000"),
+            AddFundsModel("2,000"),
+            AddFundsModel("5,000"),
+            AddFundsModel("10,000")
+        )
+        adapter?.appendData(fundList)
     }
 
     companion object {
         fun getIntent(context: Context): Intent {
             return Intent(context, TopUpPointsActivity::class.java)
         }
+    }
+
+    override fun onItemClicked(data: AddFundsModel, position: Int) {
+        adapter?.setSelectedItem(data)
+        binding.pointsEditText.setText(data.title.toString())
     }
 }
