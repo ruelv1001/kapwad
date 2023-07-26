ee68a4b8c8a14aa6ac001ffa73a23e7eb7bd1141
Joshua Calonge <joshua.calonge@ziaplex.biz>
2023-07-26 13:30:08 +0800

    Wallet Summary and Wallet Details UI/UX

--- app/src/main/java/com/lionscare/app/ui/wallet/fragment/WalletDetailsFragment.kt
+++ app/src/main/java/com/lionscare/app/ui/wallet/fragment/WalletDetailsFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.lionscare.app.R
 import com.lionscare.app.databinding.FragmentWalletDetailsBinding
+import com.lionscare.app.ui.wallet.activity.WalletActivity
+import com.lionscare.app.utils.copyToClipboard
+import com.lionscare.app.utils.setOnSingleClickListener
 
 class WalletDetailsFragment : Fragment() {
 
     private var _binding: FragmentWalletDetailsBinding? = null
     private val binding get() = _binding!!
+    private val activity by lazy { requireActivity() as WalletActivity }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,16 +31,41 @@ class WalletDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupDetails()
         setupClickListener()
     }
 
-    private fun setupClickListener() = binding.run{
-        /*backImageView.setOnSingleClickListener {
-            requireActivity().onBackPressedDispatcher.onBackPressed()
+    private fun setupDetails() = binding.run {
+        when(activity.mode){
+            "Send Points" -> {
+                titleTextView.text = getString(R.string.wallet_send_points_summary_title)
+
+                activity.data.id?.let { recipientLayout.profileImageView.setImageResource(it) }
+                recipientLayout.nameTextView.text = activity.data.title
+                recipientLayout.idNoTextView.text = activity.data.amount
+            }
+            "Scan 2 Pay" -> {
+                titleTextView.text = getString(R.string.wallet_points_recipient_summary_title)
+            }
+            "Post Request" -> {
+                titleTextView.text = getString(R.string.wallet_request_points_summary_title)
+                recipientTitleTextView.text = getString(R.string.wallet_from_title)
+
+                activity.data.id?.let { recipientLayout.profileImageView.setImageResource(it) }
+                recipientLayout.nameTextView.text = activity.data.title
+                recipientLayout.idNoTextView.text = activity.data.amount
+            }
         }
-        continueButton.setOnSingleClickListener {
-            findNavController().navigate(TopUpFragmentDirections.actionNavigationTopUpToNavigationPaymentMethod())
-        }*/
+
+    }
+
+    private fun setupClickListener() = binding.run{
+        closeImageView.setOnSingleClickListener {
+            requireActivity().finish()
+        }
+        copyImageView.setOnSingleClickListener {
+            requireContext().copyToClipboard(refidTextView.text)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/lionscare/app/ui/wallet/fragment/WalletSummaryFragment.kt
+++ app/src/main/java/com/lionscare/app/ui/wallet/fragment/WalletSummaryFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.lionscare.app.R
 import com.lionscare.app.databinding.FragmentWalletSummaryBinding
+import com.lionscare.app.ui.wallet.activity.WalletActivity
+import com.lionscare.app.utils.setOnSingleClickListener
 
 class WalletSummaryFragment : Fragment() {
 
     private var _binding: FragmentWalletSummaryBinding? = null
     private val binding get() = _binding!!
+    private val activity by lazy { requireActivity() as WalletActivity }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,16 +31,41 @@ class WalletSummaryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupDetails()
         setupClickListener()
     }
 
+    private fun setupDetails() = binding.run {
+        when(activity.mode){
+            "Send Points" -> {
+                titleTextView.text = getString(R.string.wallet_send_points_summary_title)
+
+                activity.data.id?.let { recipientLayout.profileImageView.setImageResource(it) }
+                recipientLayout.nameTextView.text = activity.data.title
+                recipientLayout.idNoTextView.text = activity.data.amount
+            }
+            "Scan 2 Pay" -> {
+                titleTextView.text = getString(R.string.wallet_points_recipient_summary_title)
+            }
+            "Post Request" -> {
+                titleTextView.text = getString(R.string.wallet_request_points_summary_title)
+                recipientTitleTextView.text = getString(R.string.wallet_from_title)
+
+                activity.data.id?.let { recipientLayout.profileImageView.setImageResource(it) }
+                recipientLayout.nameTextView.text = activity.data.title
+                recipientLayout.idNoTextView.text = activity.data.amount
+            }
+        }
+
+    }
+
     private fun setupClickListener() = binding.run{
-        /*backImageView.setOnSingleClickListener {
+        backImageView.setOnSingleClickListener {
             requireActivity().onBackPressedDispatcher.onBackPressed()
         }
         continueButton.setOnSingleClickListener {
-            findNavController().navigate(TopUpFragmentDirections.actionNavigationTopUpToNavigationPaymentMethod())
-        }*/
+            findNavController().navigate(WalletSummaryFragmentDirections.actionNavigationWalletSummaryToNavigationWalletDetails())
+        }
     }
 
     override fun onDestroyView() {
