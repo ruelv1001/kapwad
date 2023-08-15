30aec408f7d52e0b81e26468aa78bdf16ae414e9
Joshua Calonge <joshua.calonge@ziaplex.biz>
2023-08-15 18:57:44 +0800

    upload id ui/ux

---
+++ app/src/main/java/com/lionscare/app/ui/verify/fragment/UploadIDFragment.kt
@@ -0,0 +1,210 @@
+package com.lionscare.app.ui.verify.fragment
+
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import com.bumptech.glide.Glide
+import com.lionscare.app.R
+import com.lionscare.app.databinding.FragmentUploadIdBinding
+import com.lionscare.app.utils.getFileFromUri
+import com.lionscare.app.utils.setOnSingleClickListener
+import java.io.File
+import java.util.Calendar
+
+
+class UploadIDFragment : Fragment() {
+
+    private var _binding: FragmentUploadIdBinding? = null
+    private val binding get() = _binding!!
+    private var uriFilePath: Uri? = null
+    private var isBackImage = false
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentUploadIdBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupClickListener()
+        setSpinner()
+    }
+
+    private fun setSpinner() = binding.run {
+        val adapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(
+            requireActivity(),
+            R.array.id_type_items,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        idTypeSpinner.adapter = adapter
+        idTypeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedItem = parent.getItemAtPosition(position).toString()
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
+    }
+
+    private fun setupClickListener() = binding.run {
+        frontIdRelativeLayout.setOnSingleClickListener {
+            isBackImage = false
+            openMediaOptionPicker()
+        }
+        backIdRelativeLayout.setOnSingleClickListener {
+            isBackImage = true
+            openMediaOptionPicker()
+        }
+        continueButton.setOnSingleClickListener {
+
+        }
+    }
+
+    private fun openMediaOptionPicker() {
+        val choices = arrayOf("Camera", "Gallery")
+        val mBuilder = AlertDialog.Builder(requireActivity())
+        mBuilder.setTitle("Choose an action")
+        mBuilder.setSingleChoiceItems(choices, -1) { dialogInterface: DialogInterface, i: Int ->
+            if (choices[i].equals("Camera")) {
+                openCameraChecker()
+            } else {
+                openPhotoPicker()
+            }
+            dialogInterface.dismiss()
+        }
+        mBuilder.setNeutralButton("Cancel") { dialogInterface: DialogInterface, which: Int ->
+            dialogInterface.cancel()
+        }
+        val mDialog = mBuilder.create()
+        mDialog.show()
+    }
+
+    private fun openPhotoPicker(){
+        singlePhotoPickerLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val takePicture = registerForActivityResult(ActivityResultContracts.TakePicture()) { isSaved ->
+        if (isSaved)
+        {
+            if (isBackImage){
+                Glide.with(this)
+                    .load(uriFilePath)
+                    .into(binding.backIdImageView)
+               // viewModel.backIdFile = getFileFromUri(requireActivity(), uriFilePath)
+            }else {
+                Glide.with(this)
+                    .load(uriFilePath)
+                    .into(binding.frontIdImageView)
+               // viewModel.frontIdFile = getFileFromUri(requireActivity(), uriFilePath)
+            }
+        }
+    }
+
+    private val singlePhotoPickerLauncher = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { imageUri: Uri? ->
+        imageUri?.let { uri ->
+            if (isBackImage){
+                Glide.with(this)
+                    .load(uri)
+                    .into(binding.backIdImageView)
+               // viewModel.backIdFile = getFileFromUri(requireActivity(), uri)
+            }else{
+                Glide.with(this)
+                    .load(uri)
+                    .into(binding.frontIdImageView)
+               // viewModel.frontIdFile = getFileFromUri(requireActivity(), uri)
+            }
+
+        }
+    }
+
+    private fun openCameraChecker() {
+        if (hasImageCapturePermissions(requireActivity())) {
+            val calendar = Calendar.getInstance()
+            val fileName = File.createTempFile(
+                "IMG_" + calendar.timeInMillis,
+                ".jpg",
+                requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+            )
+
+            uriFilePath = FileProvider.getUriForFile(
+                requireActivity(),
+                "${requireActivity().packageName}.provider",
+                fileName
+            )
+
+            takePicture.launch(uriFilePath)
+
+        } else {
+            requestPermission()
+        }
+    }
+
+    private fun hasImageCapturePermissions(context: Activity): Boolean {
+        return !(ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.CAMERA
+        ) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(), arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            REQUEST_IMAGE_CAPTURE
+        )
+    }
+
+    override fun onRequestPermissionsResult (
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    )  = binding.run {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            if (grantResults[0] == 0) {
+                openCameraChecker()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        private const val REQUEST_IMAGE_CAPTURE = 1
+    }
+}
--- app/src/main/java/com/lionscare/app/utils/ImageUtils.kt
+++ app/src/main/java/com/lionscare/app/utils/ImageUtils.kt
@@ -3,6 +3,13 @@ package com.lionscare.app.utils
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
+import android.provider.DocumentsContract
+import android.provider.MediaStore
+import android.provider.OpenableColumns
+import android.util.Log
+import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
 import java.io.InputStream
 
@@ -19,4 +26,102 @@ fun getBitmapFromAsset(context: Context, imageName: String): Bitmap? {
     }
 
     return bitmap
+}
+
+fun getFileFromUri(context: Context, uri: Uri?): File? {
+    uri ?: return null
+    uri.path ?: return null
+    var newUriString = uri.toString()
+    newUriString = newUriString.replace(
+        "content://com.android.providers.downloads.documents/",
+        "content://com.android.providers.media.documents/"
+    )
+    newUriString = newUriString.replace(
+        "/msf%3A", "/image%3A"
+    )
+    val newUri = Uri.parse(newUriString)
+    var realPath = String()
+    val databaseUri: Uri
+    val selection: String?
+    val selectionArgs: Array<String>?
+    if (newUri.path?.contains("/document/image:") == true) {
+        databaseUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        selection = "_id=?"
+        selectionArgs = arrayOf(DocumentsContract.getDocumentId(newUri).split(":")[1])
+    } else {
+        databaseUri = newUri
+        selection = null
+        selectionArgs = null
+    }
+    try {
+//        val column = "_data"
+//        val projection = arrayOf(column)
+//        val cursor = context.contentResolver.query(
+//            databaseUri,
+//            projection,
+//            selection,
+//            selectionArgs,
+//            null
+//        )
+//        cursor?.let {
+//            if (it.moveToFirst()) {
+//                val columnIndex = cursor.getColumnIndexOrThrow(column)
+//                realPath = cursor.getString(columnIndex)
+//            }
+//            cursor.close()
+//        }
+        realPath = getRealPathFromURI(uri,context)
+
+    } catch (e: Exception) {
+        Log.i("GetFileUri Exception:", e.message ?: "")
+    }
+    val path = realPath.ifEmpty {
+        when {
+            newUri.path?.contains("/document/raw:") == true -> newUri.path?.replace(
+                "/document/raw:",
+                ""
+            )
+            newUri.path?.contains("/document/primary:") == true -> newUri.path?.replace(
+                "/document/primary:",
+                "/storage/emulated/0/"
+            )
+            else -> return null
+        }
+    }
+    return if (path.isNullOrEmpty()) null else File(path)
+}
+
+fun getRealPathFromURI(uri: Uri, context: Context): String {
+    val returnCursor = context.contentResolver.query(uri, null, null, null, null)
+    val nameIndex =  returnCursor!!.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+    val sizeIndex = returnCursor.getColumnIndex(OpenableColumns.SIZE)
+    returnCursor.moveToFirst()
+    val name = returnCursor.getString(nameIndex)
+    val size = returnCursor.getLong(sizeIndex).toString()
+    val file = File(context.filesDir, name)
+    try {
+        val inputStream: InputStream? = context.contentResolver.openInputStream(uri)
+        val outputStream = FileOutputStream(file)
+        var read = 0
+        val maxBufferSize = 1 * 1024 * 1024
+        val bytesAvailable: Int = inputStream?.available() ?: 0
+        //int bufferSize = 1024;
+        val bufferSize = Math.min(bytesAvailable, maxBufferSize)
+        val buffers = ByteArray(bufferSize)
+        while (inputStream?.read(buffers).also {
+                if (it != null) {
+                    read = it
+                }
+            } != -1) {
+            outputStream.write(buffers, 0, read)
+        }
+        Log.e("File Size", "Size " + file.length())
+        inputStream?.close()
+        outputStream.close()
+        Log.e("File Path", "Path " + file.path)
+
+    } catch (e: java.lang.Exception) {
+        Log.e("Exception", e.message!!)
+    }
+    return file.path
 }
