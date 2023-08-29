2366bc4b981bed7a99a94837815db886c25f4f5b
leojangelicomacario <leoj.macario@mylyka.com>
2023-08-29 18:04:15 +0800

    Setup Wallet Viewstate and ViewModel

---
+++ app/src/main/java/com/lionscare/app/ui/wallet/viewmodel/WalletViewState.kt
@@ -0,0 +1,26 @@
+package com.lionscare.app.ui.wallet.viewmodel
+
+import androidx.paging.PagingData
+import com.lionscare.app.data.model.ErrorsData
+import com.lionscare.app.data.repositories.group.response.CreateGroupResponse
+import com.lionscare.app.data.repositories.wallet.response.BalanceData
+import com.lionscare.app.data.repositories.wallet.response.GetBalanceResponse
+import com.lionscare.app.data.repositories.wallet.response.TransactionData
+import com.lionscare.app.utils.PopupErrorState
+
+sealed class WalletViewState{
+
+    object Loading : WalletViewState()
+    data class SuccessGetBalance(val balanceData: BalanceData? = null) : WalletViewState()
+
+    data class SuccessTopup(val webUrl: String? = null) : WalletViewState()
+
+    data class SuccessTransactionDetail(val details: TransactionData? = null) : WalletViewState()
+
+    data class SuccessTransactionList(val pagingData: PagingData<TransactionData>) : WalletViewState()
+
+    data class PopupError(val errorCode: PopupErrorState, val message: String = "") : WalletViewState()
+
+    data class InputError(val errorData: ErrorsData? = null) : WalletViewState()
+
+}
