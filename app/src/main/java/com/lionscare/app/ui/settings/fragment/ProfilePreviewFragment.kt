691d2da92c1e4d4f5fc8548c5f7c39bcfe62f406
denuden <79180351+denuden@users.noreply.github.com>
2023-09-05 14:33:38 +0800

    setup fragments

--- app/src/main/java/com/lionscare/app/ui/settings/fragment/ProfilePreviewFragment.kt
+++ app/src/main/java/com/lionscare/app/ui/settings/fragment/ProfilePreviewFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -46,9 +47,9 @@ class ProfilePreviewFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         observeProfile()
         setClickListeners()
-        onResume()
         viewModel.getProfileDetails()
     }
+
 
     private fun observeProfile() {
         viewLifecycleOwner.lifecycleScope.launch {
