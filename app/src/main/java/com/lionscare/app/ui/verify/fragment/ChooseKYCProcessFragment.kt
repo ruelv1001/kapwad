4b60a4bbb63fe5aa9edc22003230b61e3eb99ca6
denuden <79180351+denuden@users.noreply.github.com>
2023-09-05 21:12:42 +0800

    text files

--- app/src/main/java/com/lionscare/app/ui/verify/fragment/ChooseKYCProcessFragment.kt
+++ app/src/main/java/com/lionscare/app/ui/verify/fragment/ChooseKYCProcessFragment.kt
@@ -5,11 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.lionscare.app.R
 import com.lionscare.app.databinding.FragmentChooseKycProcessBinding
+import com.lionscare.app.ui.settings.viewmodel.ProfileViewState
+import com.lionscare.app.ui.verify.VerifyViewModel
 import com.lionscare.app.ui.verify.activity.AccountVerificationActivity
 import com.lionscare.app.utils.setOnSingleClickListener
+import com.lionscare.app.utils.showPopupError
+import kotlinx.coroutines.launch
 
 class ChooseKYCProcessFragment : Fragment() {
 
@@ -18,6 +27,8 @@ class ChooseKYCProcessFragment : Fragment() {
     private var isIdVerified : Boolean? = false
     private var isAddressVerified : Boolean? = false
     private val activity by lazy { requireActivity() as AccountVerificationActivity }
+
+    private val viewModel : VerifyViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,9 +44,83 @@ class ChooseKYCProcessFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        observeProfile()
         setupClickListener()
         setUpDetails()
+        viewModel.getVerificationStatus()
+
     }
+
+
+
+    private fun observeProfile() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.kycSharedFlow.collect { viewState ->
+                    handleViewState(viewState)
+                }
+            }
+        }
+    }
+
+
+    private fun handleViewState(viewState: ProfileViewState) {
+        when (viewState) {
+            is ProfileViewState.Loading -> Unit
+            is ProfileViewState.SuccessGetVerificationStatus -> {
+                if (viewState.profileVerificationResponse.data?.id_status == "pending"
+                    && viewState.profileVerificationResponse.data?.address_status == "pending"
+                    ){
+                    binding.statusTextView.text = "Pending"
+
+                    binding.verifyTextView.text = "ID Status: ${viewState.profileVerificationResponse.data?.id_status }\n" +
+                            "ID Type: ${viewState.profileVerificationResponse.data?.id_type}\n" +
+                            "Submitted on: ${viewState.profileVerificationResponse.data?.id_submitted_date?.date_db}"
+
+                    binding.verifyAddressTextView.text = "Address Proof Status: ${viewState.profileVerificationResponse.data?.address_status }\n" +
+                            "Address Proof Type: ${viewState.profileVerificationResponse.data?.address_type}\n" +
+                            "Submitted on: ${viewState.profileVerificationResponse.data?.address_submitted_date?.date_db}"
+                }
+
+                if (viewState.profileVerificationResponse.data?.id_status == "approved"
+                    && viewState.profileVerificationResponse.data?.address_status == "approved"
+                ){
+                    binding.statusTextView.text = "Approved"
+
+                    binding.verifyTextView.text = "ID Status: ${viewState.profileVerificationResponse.data?.id_status }\n" +
+                            "ID Type: ${viewState.profileVerificationResponse.data?.id_type}\n" +
+                            "Submitted on: ${viewState.profileVerificationResponse.data?.id_submitted_date?.date_db}"
+
+                    binding.verifyAddressTextView.text = "Address Proof Status: ${viewState.profileVerificationResponse.data?.address_status }\n" +
+                            "Address Proof Type: ${viewState.profileVerificationResponse.data?.address_type}\n" +
+                            "Submitted on: ${viewState.profileVerificationResponse.data?.address_submitted_date?.date_db}"
+                }
+
+
+                if (viewState.profileVerificationResponse.data?.id_status == "declined"
+                    && viewState.profileVerificationResponse.data?.address_status == "declined"
+                ){
+                    binding.statusTextView.text = "Declined"
+
+                    binding.verifyTextView.text = "ID Status: ${viewState.profileVerificationResponse.data?.id_status }\n" +
+                            "ID Type: ${viewState.profileVerificationResponse.data?.id_type}\n" +
+                            "Submitted on: ${viewState.profileVerificationResponse.data?.id_submitted_date?.date_db}"
+
+                    binding.verifyAddressTextView.text = "Address Proof Status: ${viewState.profileVerificationResponse.data?.address_status }\n" +
+                            "Address Proof Type: ${viewState.profileVerificationResponse.data?.address_type}\n" +
+                            "Submitted on: ${viewState.profileVerificationResponse.data?.address_submitted_date?.date_db}"
+                }
+
+
+            }
+            is ProfileViewState.PopupError -> {
+                showPopupError(requireContext(), childFragmentManager, viewState.errorCode, viewState.message)
+            }
+            else -> Unit
+        }
+    }
+
+
 
     private fun setUpDetails() = binding.run{
         activity.setTitle(getString(R.string.verify_account_title))
